25e6a2301e7d6232a0f17d66fbdde0dae740b041
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-07-01 08:48:32 -0700

    separate business logic from UI, add description to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/weather-interface.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
@@ -19,7 +19,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: '',
+      title: 'weather project',
       template: './src/index.html',
       inject: 'body'
     }), 
